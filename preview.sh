9c0ef623cd14a4659101800ada4ed8c7571b0173
temeteke <temeteke@gmail.com>
2023-09-16 14:27:19 +0900

    Change preview option

--- preview.sh
+++ preview.sh
@@ -1,9 +1,9 @@
 if type bat > /dev/null 2>&1; then
-    export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always {} 2>/dev/null || ls {}'"
+    export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always {} 2>/dev/null || \ls -Alh --color=always --time-style=long-iso {}'"
 fi
 
 if type tree > /dev/null 2>&1; then
     export FZF_ALT_C_OPTS="--preview 'tree -C {}'"
 else
-    export FZF_ALT_C_OPTS="--preview 'ls {}'"
+    export FZF_ALT_C_OPTS="--preview '\ls -AlFG --color=always --time-style=long-iso {}'"
 fi
